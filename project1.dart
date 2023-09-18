f40a73ff0f51df9a48578d71fdd7a7c929af2789
yahya <yahyasiddiqui146@gmail.com>
2023-09-19 00:24:26 +0500

    hello world

---
+++ project1.dart
@@ -0,0 +1,5 @@
+void main() {
+  //Q.1:
+
+  var listnumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
---
+++ project5.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Q.6:
+
+  for (int table = 5; table <= 5; table++) {
+    print("Table of $table");
+    for (var i = 1; i <= 10; i++) {
+      print("$i x $table = ${i * table}");
+    }
+  }
+}
